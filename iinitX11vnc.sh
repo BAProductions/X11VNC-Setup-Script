33f655c30d47405309e7a42ff126df68bda86cc0
DJABHipHop <djabhiphop-DJABHipHop@yahoo.com>
2020-09-30 16:51:46 -0400

    Asking?
    Asking?

--- iinitX11vnc.sh
+++ iinitX11vnc.sh
@@ -16,17 +16,36 @@ function isPackageInstalled() {
 
 # Setup X11vnc, net-tools
 function SetupX11vnc() {
-    apt-get update
-    isPackageInstalled x11vnc
-    isPackageInstalled net-tools
-    sudo "echo "@reboot x11vnc" >> /var/spool/cron/root"
+    read -p "Do You Install X11VNC y/n?" -n 1 -r
+    echo    # Do Wou Want Make X11VNC Discover On MacOS?
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Seting UP X11vnc"
+        apt-get update
+        isPackageInstalled x11vnc
+        isPackageInstalled net-tools
+        # read -p "Do Wou Want Make X11VNC Discover On MacOS y/n?" -n 1 -r
+        # echo    # Do Wou Want Make X11VNC Discover On MacOS?
+        # if [[ $REPLY =~ ^[Yy]$ ]]
+        # then
+        echo "Adding x11vnc to >> /var/spool/cron/root"
+        sudo echo "@reboot x11vnc" >> /var/spool/cron/root
+        #fi
+    fi
 }
 
 # Setup avahi-daemon
 function SetupAvahiDaemon() {
-    isPackageInstalled avahi-daemon
-    sudo cp sercives/rfb.service /etc/avahi/services/
-    sudo systemctl restart avahi-daemon
+    read -p "Do You Want Make X11VNC Discover On MacOS y/n?" -n 1 -r
+    echo    # Do Wou Want Make X11VNC Discover On MacOS?
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        isPackageInstalled avahi-daemon
+        echo "Coping sercives/rfb.service /etc/avahi/services/"
+        sudo cp sercives/rfb.service /etc/avahi/services/
+        echo "Restarting avahi-daemon"
+        sudo systemctl restart avahi-daemon
+    fi
 }
 
 # init
